23d6aafe8d01a0930509d8c2ae162f1b90e3caf4
Kaung Myat Soe <52688252+KaungMyatSoe11@users.noreply.github.com>
2024-06-05 12:53:07 +0630

    init

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,10 @@
 import { Inter } from "next/font/google";
+
 import "./globals.css";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import { Toaster } from "@/components/ui/sonner";
+import Providers from "./providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +16,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Providers>
+          <Header />
+          <div className="container mx-auto px-5">{children}</div>
+          <Footer />
+          <Toaster />
+        </Providers>
+      </body>
     </html>
   );
 }
